bc911c5a9d3b3baa0dbe36d8604a7507f54916b5
Gabriel Semonis <gsemonis@gmail.com>
2025-04-19 17:08:20 -0500

    finished using mouse to crop area

--- week_2/scriptFiles/5_using_the_mouse_for_annotation.py
+++ week_2/scriptFiles/5_using_the_mouse_for_annotation.py
@@ -1,50 +1,70 @@
 import cv2
+import numpy as np
 import math
 
 # Lists to store the points
-center=[]
-circumference=[]
+down_point = (-1, -1)
+up_point = (-1,1)
+drawing = False
 
-def drawCircle(action, x, y, flags, userdata):
-  # Referencing global variables 
-  global center, circumference
-  # Action to be taken when left mouse button is pressed
-  if action==cv2.EVENT_LBUTTONDOWN:
-    center=[(x,y)]
-    # Mark the center
-    cv2.circle(source, center[0], 1, (255,255,0), 2, cv2.LINE_AA )
+def draw_rectangle(action, x, y, flags, userdata):
 
-  # Action to be taken when left mouse button is released
+  global down_point, up_point, drawing, source, source_copy, over_lay
+
+  # left button down
+  if action == cv2.EVENT_LBUTTONDOWN:
+    drawing = True
+    down_point = (x,y)
+    up_point = (x,y)
+
+  elif action == cv2.EVENT_MOUSEMOVE:
+    if drawing:
+      up_point = (x,y)
+      over_lay = np.zeros_like(source)
+      cv2.rectangle(over_lay, down_point, up_point, (255,0,255), 3, lineType = cv2.LINE_AA)
+      source_copy = cv2.add(source, over_lay)
+      cv2.imshow("Window", source_copy)
+
+  # left button up
   elif action==cv2.EVENT_LBUTTONUP:
-    circumference=[(x,y)]
-    # Calculate radius of the circle
-    radius = math.sqrt(math.pow(center[0][0]-circumference[0][0],2)+
-                        math.pow(center[0][1]-circumference[0][1],2))
-    # Draw the circle
-    cv2.circle(source, center[0], int(radius), (0,255,0),2, 
-                    cv2.LINE_AA)
-    cv2.imshow("Window",source)
+    drawing = False
+    up_point = (x,y)
+    cv2.rectangle(over_lay, down_point, up_point,(255,0,255), 3, cv2.LINE_AA)
+    source_copy = cv2.add(source, over_lay)
+    cv2.imshow("Window", source_copy)
+
+    x1, y1 = down_point
+    x2, y2 = up_point
+
+    x_min, x_max = sorted([x1, x2])
+    y_min, y_max = sorted([y1, y2])
+
+    crop_area = source_copy[y_min:y_max, x_min:x_max]
+    cv2.imwrite("croppedArea.jpg", crop_area)
+    #cv2.imshow("cropped area", crop_area)
 
 
-source = cv2.imread("../data/images/sample.jpg",1)
-# Make a dummy image, will be useful to clear the drawing
-dummy = source.copy()
+
+source = cv2.imread("../data/images/sample.jpg", cv2.IMREAD_COLOR)
+cv2.putText(source, '''Choose corner, and drag, Press ESC to exit and c to clear''', (10, 30),
+              cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+source_copy = source.copy()
+
+#make an overlay for drawing on
+over_lay = np.zeros_like(source)
+
 cv2.namedWindow("Window")
+
 # highgui function called when mouse events occur
-cv2.setMouseCallback("Window", drawCircle)
+cv2.setMouseCallback("Window", draw_rectangle)
 k = 0
 # loop until escape character is pressed
-while k!=27 :
-  
-  cv2.imshow("Window", source)
-  cv2.putText(source,'''Choose center, and drag, 
-                      Press ESC to exit and c to clear''' ,
-              (10,30), cv2.FONT_HERSHEY_SIMPLEX, 
-              0.7,(255,255,255), 2 );
-  k = cv2.waitKey(20) & 0xFF
-  # Another way of cloning
-  if k==99:
-    source= dummy.copy()
+while k != 27 :
+  cv2.imshow("Window", source_copy)
 
+  k = cv2.waitKey(20) & 0xFF
+  if k==99:
+    over_lay = np.zeros_like(source)
+    source_copy = source.copy()
 
 cv2.destroyAllWindows()
